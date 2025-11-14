a2a0b29ff6ee8f8c01dbdd0645c13a581533a807
Tu Nombre <danielachaparro@z1.digital>
2025-11-14 10:24:34 +0100

    feat(views_graphql): implement custom view for graphql middleware

--- blog/auth_app/views_graphql.py
+++ blog/auth_app/views_graphql.py
@@ -1,16 +1,24 @@
 from graphene_django.views import GraphQLView  # pyright: ignore[reportMissingImports]
-from graphql_jwt.middleware import (  # pyright: ignore[reportMissingImports]
-    JSONWebTokenMiddleware,  # pyright: ignore[reportMissingImports]
+from graphql_jwt.shortcuts import (  # pyright: ignore[reportMissingImports]
+    get_user_by_token,  # pyright: ignore[reportMissingImports]
 )
+
+from blog_app.constants import ERROR_NOT_OBTAIN_USER_BYTOKEN
 
 
 class CustomGraphQLView(GraphQLView):
-    def __init__(self, *args, **kwargs):
-        middleware = kwargs.pop("middleware", [])
-        middleware.append(JSONWebTokenMiddleware())
-        kwargs["middleware"] = middleware
-        super().__init__(*args, **kwargs)
-
     def get_context(self, request):  # noqa: PLR6301
-        # Esto asegura que info.context.user sea el usuario autenticado por JWT
+        """
+        Asegura que info.context.user sea el usuario autenticado por JWT
+        si el middleware no lo resuelve automáticamente.
+        """
+        # Si el middleware no lo hace, lo intentamos manualmente:
+        auth = request.headers.get("authorization", "")
+        if auth.startswith("Bearer "):
+            token = auth.split("Bearer ")[1]
+            try:
+                user = get_user_by_token(token)
+                request.user = user
+            except Exception:
+                print(ERROR_NOT_OBTAIN_USER_BYTOKEN)
         return request
