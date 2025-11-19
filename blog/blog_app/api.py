55d7a99a9947453a9a1864ba76bf8cb4e7f62aea
Tu Nombre <danielachaparro@z1.digital>
2025-11-19 13:33:52 +0100

    refactor(api): delete comment

--- blog/blog_app/api.py
+++ blog/blog_app/api.py
@@ -58,50 +58,6 @@ class PostViewSet(viewsets.ModelViewSet):
 
 
 # ViewSet para Tag
-"""
-class TagViewSet(viewsets.ModelViewSet):
-
-    serializer_class = TagSerializer
-    permission_classes = [IsBlogOwnerOrAdmin]
-
-    def get_queryset(self):
-        user = self.request.user
-        if is_superuser(user):  # Si es superusuario, devuelve todos los tags
-            return Tag.objects.all()
-        return Tag.objects.filter(
-            posts__blog__user=user
-        ).distinct()  # Solo los tags asociados a posts cuyo blog pertenece al usuario
-
-    def perform_create(self, serializer):
-        user = self.request.user
-        validated_data = (
-            serializer.validated_data
-        )  # Obtiene los datos validados del tag
-        posts = validated_data.get("posts", [])  # Obtiene los posts del tag
-        name = validated_data.get("name")  # Obtiene el nombre del tag
-
-        try:
-            blog = user.blog
-        except Blog.DoesNotExist:
-            raise PermissionDenied("No puedes crear tags si no tienes un blog.")  # noqa: B904
-
-        # blog = get_user_blog(user)  # Obtiene el blog del usuario
-        validate_user_owns_posts(user, posts)  # Valida propiedad de posts
-        tag = get_or_create_tag_by_name(blog, name)  # Obtiene o crea tag
-
-        tag.posts.add(*posts)  # Asocia posts (evita duplicados automáticamente)
-
-        serializer.instance = tag  # Asigna la instancia creada al serializer
-
-    def get_serializer_context(
-        self,
-    ):  # Añade el objeto 'request' al contexto del serializer para permitir validaciones basadas en el usuario autenticado
-        context = super().get_serializer_context()
-        context["request"] = self.request
-        return context
-"""
-
-
 class TagViewSet(viewsets.ModelViewSet):
     serializer_class = TagSerializer
     permission_classes = [IsAuthenticated]
